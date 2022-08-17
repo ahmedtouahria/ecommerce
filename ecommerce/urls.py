4af44ed801c198d403e28cafbc568f791fad90fb
Touahria Ahmed <ahmedtouahria2001@gmail.com>
2022-08-17 16:28:19 +0200

    deploy v1.1

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,11 +6,12 @@ urlpatterns = [
     path('admin/', admin.site.urls,name='admin'),
     path('',include('shopping.urls')),
     path('dashboard/',include('dashboard.urls')),
-    
     path('api/',include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-     path('oauth/', include('social_django.urls', namespace='social')), 
-         path('accounts/', include('allauth.urls')),
-
+    path('oauth/', include('social_django.urls', namespace='social')), 
+    path('accounts/', include('allauth.urls')),
   #  path('',include('user.urls')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
