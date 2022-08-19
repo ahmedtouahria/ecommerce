eee10e88e5ae0c3466e555debf5b0938c3df4669
Touahria Ahmed <ahmedtouahria2001@gmail.com>
2022-08-19 15:39:14 +0200

    staticfiles conf

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -227,10 +227,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'ecommerce/static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # 'data' is my media folder
